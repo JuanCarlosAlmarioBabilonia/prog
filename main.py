c433e1bd62ba32605183adcb2767c488b39fd846
JuanCarlosAlmarioBabilonia <160683492+JuanCarlosAlmarioBabilonia@users.noreply.github.com>
2024-02-26 13:48:31 -0500

    Ejercicio Tuple

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-info_usuario = input ("Ingrese su(s) nombre(s): "), input ("Ingrese sus apellidos: "), input ("Ingrese su edad: ")
-direcciones = input ("Ingrese su direccion principal (hogar):"), input ("Ingrese su direccion laboral (trabajo): ")
-educacion= input ("Ingrese el/los nivel(es) de educacion recibida (preescolar, basica, media o superior): "), input ("Ingrese los cursos que ha realizado: "), input ("Ingrese los titulos que ha obtenido: ")
+#info_usuario
+nombre = input ("Ingrese su(s) nombre(s): ")
+apellidos = input ("Ingrese sus apellidos: ")
+edad = input ("Ingrese su edad: ")
+altura = input ("Ingrese su altura en metros: ")
+info = tuple ((nombre, apellidos, edad, altura))
+print (f"El/La usuario/a {info[0]} {info[1]} tiene {info[2]} y mide {info[3]}")
+
+#direcciones
+d_casa = input ("Ingrese su direccion principal (hogar):") 
+d_trabajo = input ("Ingrese su direccion laboral (trabajo): ")
+dire = tuple ((d_casa, d_trabajo))
+print (f"El/La usuario/a {info[0]} {info[1]} vive en {dire[0]} y labora en {dire[1]}")
+
+#educacion
+nvl_edu = input ("Ingrese el/los nivel(es) de educacion recibida (preescolar, basica, media o superior): ")
+cursos = input ("Ingrese el/los curso(s) que ha realizado: ")
+titulos = input ("Ingrese el/los titulo(s) que ha obtenido: ")
+edu = tuple ((nvl_edu, cursos, titulos))
+print (f"El/La usuario/a {info[0]} {info[1]} recibio educacion {edu[0]}, realizo un/os curso(s) de {edu[1]} y posee un/os titulo(s) como {edu[2]}  ")
+
+#exp_laboral
+sn = input ("¿Ha tenido experiencia laboral? (responda si o no): ")
 exp_laboral= input ("Ingrese su experiencia laboral: ")
-info = tuple ((info_usuario))
-dire = tuple ((direcciones))
-edu = tuple ((educacion))
-exp = tuple ((exp_laboral))
-print (info)
+exp = tuple ((sn, exp_laboral))
+print (f"El/La usuario/a {info[0]} {info[1]} {exp[0]} ha tenido experiencia laboral como {exp[1]} ")
